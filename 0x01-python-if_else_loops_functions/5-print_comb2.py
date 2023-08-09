2a9ef582408e3612c21703406769fd02b81108fb
Wangui-quinnie <w.wangui@ymail.com>
2023-08-09 05:57:18 -0700

    correction to get the expected output

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    print(F"{i:02}", end=", ")
-    if i == 99:
-        print()
+for i in range(99):
+    print("{:02d}".format(i), end=", ")
+print("{:02d}".format(99))
