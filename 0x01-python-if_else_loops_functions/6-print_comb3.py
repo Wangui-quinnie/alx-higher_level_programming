2756609c89b9821a817b66649e39d335c9035664
Wangui-quinnie <w.wangui@ymail.com>
2023-08-09 09:20:38 -0700

    adjusted my range to fix the output to the expected one

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(1, 10):
+for i in range(9):
+    for j in range(i+1, 10):
         if i * 10 + j < 89:
             print("{}{}".format(i, j), end=", ")
 print("{}".format(89))
