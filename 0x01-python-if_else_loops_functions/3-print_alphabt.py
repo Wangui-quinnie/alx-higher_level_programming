488baa6336939aa66d3b2cc72e8a77e2c3a524b4
Wangui-quinnie <w.wangui@ymail.com>
2023-08-09 06:03:07 -0700

    added a missing ' in ('z')

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet_letters in range(ord('a'), ord('z)+1):
+for alphabet_letters in range(ord('a'), ord('z')+1):
     if chr(alphabet_letters) not in ('e', 'q'):
         print("{:c}".format(alphabet_letters), end="")
