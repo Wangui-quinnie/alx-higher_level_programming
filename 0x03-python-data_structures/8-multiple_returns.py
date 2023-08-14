e56b3cc01508e746412e8d576de6558dc27198eb
Wangui-quinnie <w.wangui@ymail.com>
2023-08-14 15:10:51 -0700

    More returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        return (None, None)
+    length = len(sentence)
+    first_character = sentence[0]
+    return (length, first_character)
