293c41a31e2ab285e850c86e273e49a49c74439f
Wangui-quinnie <w.wangui@ymail.com>
2023-08-29 12:43:01 -0700

    documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,20 @@
 #!/usr/python3
+"""
+This class represents a square
+"""
+
+
 class Square:
     """
-    This class represents a square
+    The size attribute is a private instance attribute
     """
 
     def __init__(self, size):
         """
         Initialize a square object with a given size
+
         Args:
             size (int): The length of the square
         """
+
         self.__size = size
