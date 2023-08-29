1cc3eef997e531db448b1f30259f6fe34ebcb2a3
Wangui-quinnie <w.wangui@ymail.com>
2023-08-29 12:29:46 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/python3
+class Square:
+    """
+    This class represents a square
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a square object with a given size
+        Args:
+            size (int): The length of the square
+        """
+        self.__size = size
