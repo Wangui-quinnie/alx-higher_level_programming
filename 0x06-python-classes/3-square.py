33b8e64384ce2ab5afe051c5d8961ff1eea20987
Wangui-quinnie <w.wangui@ymail.com>
2023-08-29 15:48:43 -0700

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This class represents a Square."""
+
+
+class Square:
+    """Define an attribute representing the length of the square."""
+
+    def __init__(self, size=0):
+        """Initialize a Square object with size.
+
+        Args:
+            size (int): The length of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
