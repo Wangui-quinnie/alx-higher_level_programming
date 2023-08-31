72bd7ff11086bea91bf6f0bb47d4153ea7f2efb7
Wangui-quinnie <w.wangui@ymail.com>
2023-08-30 18:24:54 -0700

    refactored the code

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
-    except Exception as e:
-        print("Exception:", e, file=sys.stderr)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
