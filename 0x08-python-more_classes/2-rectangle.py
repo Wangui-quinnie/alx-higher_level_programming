4d54a4b41a5d3c66095201c16e989b6365b18584
Wangui-quinnie <w.wangui@ymail.com>
2023-09-04 15:42:02 -0700

    changed the conditional statement

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         """Set the width of the rectangle with error checking."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -35,7 +35,7 @@ class Rectangle:
         """Set the height of the rectangle with error checking."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
