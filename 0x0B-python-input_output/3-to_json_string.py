b6e8c02d969bd6f18f54116631ffa4343aad91ff
Wangui-quinnie <w.wangui@ymail.com>
2023-09-12 16:10:40 -0700

    To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,5 +4,10 @@ import json
 
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object."""
+    """Converts my_obj to the JSON representation.
+
+    my_obj: the object to be converted.
+
+    Returns the JSON representation string.
+    """
     return json.dumps(my_obj)
