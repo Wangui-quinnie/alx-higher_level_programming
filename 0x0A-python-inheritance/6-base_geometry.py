1055ff83bfd3c8319d4bdac79cafdab48f4997e1
Wangui-quinnie <w.wangui@ymail.com>
2023-09-11 17:18:59 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents a class basegeometry."""
+    def area(self):
+        """Raises an exception."""
+        raise Exception("area() is not implemented")
