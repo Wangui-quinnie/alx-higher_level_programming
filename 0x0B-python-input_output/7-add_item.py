21f65a776f104e6fe5df9fdd865e5d8400908d04
Wangui-quinnie <w.wangui@ymail.com>
2023-09-12 18:18:59 -0700

    added documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Adds all arguments to a python list and saves them in a file."""
+
+
 import sys
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file')\
