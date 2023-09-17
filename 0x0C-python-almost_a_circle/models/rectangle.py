52ac87ba84f327f0d991afafa4bbbf944a4a24bf
Wangui-quinnie <w.wangui@ymail.com>
2023-09-17 03:46:27 -0700

    First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from Base."""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represents a class Rectangle that inherits from Base."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a new Rectangle object.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle's position.
+            y (int, optional): The y-coordinate of the rectangle's position.
+            id (int, optional): The id of the rectangle. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter method for width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width attribute."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height attribute."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter method for x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter method for x attribute."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter method for y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter method for y attribute."""
+        self.__y = value
