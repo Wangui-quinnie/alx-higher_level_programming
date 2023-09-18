c1c6ab1c9043b189844cb2ca97cd40e456c81d70
Wangui-quinnie <w.wangui@ymail.com>
2023-09-18 01:19:42 -0700

    Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -127,9 +127,14 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
-        """Displays the rectangle using '#' characters."""
+        """Displays the rectangle using '#' characters.
+
+        Taking into account the rectangle's pos (x and y).
+        """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
