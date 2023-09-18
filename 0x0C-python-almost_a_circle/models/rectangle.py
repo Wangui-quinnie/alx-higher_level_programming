9b658b410a2932949e4d5ebf0c23355fbb74d489
Wangui-quinnie <w.wangui@ymail.com>
2023-09-17 23:36:47 -0700

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -32,7 +32,20 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """Setter method for width attribute."""
+        """
+        Set the width of the rectangle.
+
+        Args:
+            Value (int): The width value to set.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value <= 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -42,7 +55,20 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """Setter method for height attribute."""
+        """
+        Set the height of the rectangle.
+
+        Args:
+            value (int): The height value to set.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value <= 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -52,7 +78,20 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        """Setter method for x attribute."""
+        """
+        Set the x-coordinate of the rectangle's position.
+
+        Args:
+            value (int): The x-coordinate value to set.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value < 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -62,5 +101,18 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        """Setter method for y attribute."""
+        """
+        Set the y-coordinate of the rectangle's position.
+
+        Args:
+            value (int): The y-coordinate value to set.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value < 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
