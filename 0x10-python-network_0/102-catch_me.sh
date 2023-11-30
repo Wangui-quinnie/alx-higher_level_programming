bbdabbe2b64616260c3d007449f0940a73c6e8f9
Wangui-quinnie <w.wangui@ymail.com>
2023-11-30 07:47:25 -0800

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -s -X PUT -L -d -H "Origin: HolbertonSchool" "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -X PUT -L -d -H "user_id=98" 0.0.0.0:5000/catch_me
