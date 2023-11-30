ac533a3463b5a63ffef6aa0ac5df475a6fd96420
Wangui-quinnie <w.wangui@ymail.com>
2023-11-30 05:48:34 -0800

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a url,sends a GET request, and displays the body of the response
-curl -s -o /dev/stdout -w "%{http_code}" "$1" | awk '{body=(NR>1?body $0"\n":$0)} END {if($0==200)print body}'
+curl -sL "$1"
