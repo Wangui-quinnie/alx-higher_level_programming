b6368e0ca990097028f465540a594de8a2e2323a
Wangui-quinnie <w.wangui@ymail.com>
2023-12-01 15:58:03 -0800

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,9 @@ import urllib.request
 import sys
 
 url = sys.argv[1]
+req = urllib.request.Request(url)
 
-with urllib.request.urlopen(url) as response:
+with urllib.request.urlopen(req) as response:
     request_id = response.headers.get('X-Request-Id')
     print(request_id)
 
