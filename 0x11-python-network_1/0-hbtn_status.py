e55e4fa5f9cc99bf0c5c9d2c73c5285d71a7a82e
Wangui-quinnie <w.wangui@ymail.com>
2023-12-01 03:05:20 -0800

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+req = urllib.request.Request(url)
+
+with urllib.request.urlopen(req) as response:
+    content = response.read()
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", content.decode('utf-8'))
