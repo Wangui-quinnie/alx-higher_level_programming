03ae64916c237474ddea0e82922f2c879394b020
Wangui-quinnie <w.wangui@ymail.com>
2023-12-01 18:46:20 -0800

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,11 +4,13 @@ Python script that fetches https://alx-intranet.hbtn.io/status.
 """
 import requests
 
-url = "https://alx-intranet.hbtn.io/status"
-response = requests.get(url)
 
-if response.status_code == 200:
-    content = response.text  # Use response.text instead of response.json()
-    print("Body response:")
-    print("\t- type:", type(content).__name__)
-    print("\t- content:", content)
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        content = response.text
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
