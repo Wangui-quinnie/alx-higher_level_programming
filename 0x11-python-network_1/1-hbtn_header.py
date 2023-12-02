212ac005d53814f42e30e4d51a1a0c41c8bb71f7
Wangui-quinnie <w.wangui@ymail.com>
2023-12-01 16:11:26 -0800

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
+"""1-hbtn_header module."""
+
+
 import urllib.request
 import sys
 
-url = sys.argv[1]
-req = urllib.request.Request(url)
 
-with urllib.request.urlopen(req) as response:
-    request_id = response.headers.get('X-Request-Id')
-    print(request_id)
+def fetch_x_request_id(url):
+    """
+    Fetches the value of the X-Request-Id variable from the header of the
+    response.
+    """
+
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        request_id = response.headers.get('X-Request-Id')
+        print(request_id)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    fetch_x_request_id(url)
