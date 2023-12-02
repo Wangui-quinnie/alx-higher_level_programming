b72f3b19e51ddd40394a85331799fc5533f96de0
Wangui-quinnie <w.wangui@ymail.com>
2023-12-01 18:35:34 -0800

    pycodestyle

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,10 +13,8 @@ if __name__ == "__main__":
     email = sys.argv[2]
 
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    request = urllib.request.Request(url, data)
 
-    try:
-        with urllib.request.urlopen(url, data=data) as response:
-            content = response.read().decode('utf-8')
-            print(content)
-    except urllib.error.HTTPError as e:
-        print("Error code:", e.code)
+    with urllib.request.urlopen(request) as response:
+        content = response.read().decode('utf-8')
+        print(content)
