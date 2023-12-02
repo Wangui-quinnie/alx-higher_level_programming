416ab6ee56fff4f2310cbe06c5513c1eeb1f9d3a
Wangui-quinnie <w.wangui@ymail.com>
2023-12-01 18:25:02 -0800

    documentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+Must use the package urllib.
+"""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-req = urllib.request.Request(url)
 
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode('utf-8'))
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type:", type(content))
+        print("\t- content:", content)
+        print("\t- utf8 content:", content.decode('utf-8'))
